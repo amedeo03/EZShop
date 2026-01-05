f3b5f2a111fa183fb8119ae41dacb187acf803a5
amedeo03 <amedeomarino03@gmail.com>
2026-01-05 21:43:12 +0100

    test: added further tests for product routes

--- tests/e2e/routes/test_products_route.py
+++ tests/e2e/routes/test_products_route.py
@@ -202,3 +202,264 @@ class TestProductsRouter:
             resp.status_code = 400
 
         assert resp.status_code == expected_exception_code
+
+    @pytest.mark.parametrize(
+        "input_barcode, expected_exception_code",
+        [
+            ("123456789012", 200),  # success
+            ("426", 400),  # invalid barcode
+            ("4260123456788", 404),  # no product found
+            ("", 400),  # invalid id
+        ],
+    )
+    def test_get_product_by_barcode(
+        self, client, auth_tokens, input_barcode, expected_exception_code
+    ):
+
+        headers = auth_header(auth_tokens, "admin")
+        product = {
+            "description": "Test product",
+            "barcode": "123456789012",
+            "price_per_unit": 9.99,
+            "note": "",
+            "quantity": 10,
+            "position": "A-1-1",
+        }
+
+        client.post(
+            BASE_URL + "/products",
+            json=product,
+            headers=headers,
+        )
+
+        resp = client.get(
+            BASE_URL + "/products/barcode/" + input_barcode,
+            headers=headers,
+        )
+
+        if resp.status_code == 422:
+            resp.status_code = 400
+
+        assert resp.status_code == expected_exception_code
+
+    @pytest.mark.parametrize(
+        "input_description, expected_exception_code",
+        [
+            ("Test", 200),  # success
+            ("tseT", 404),  # no product found
+        ],
+    )
+    def test_get_product_by_description(
+        self, client, auth_tokens, input_description, expected_exception_code
+    ):
+
+        headers = auth_header(auth_tokens, "admin")
+        product = {
+            "description": "Test product",
+            "barcode": "123456789012",
+            "price_per_unit": 9.99,
+            "note": "",
+            "quantity": 10,
+            "position": "A-1-1",
+        }
+
+        client.post(
+            BASE_URL + "/products",
+            json=product,
+            headers=headers,
+        )
+
+        resp = client.get(
+            BASE_URL + "/products/search?query=" + input_description,
+            headers=headers,
+        )
+
+        if resp.status_code == 422:
+            resp.status_code = 400
+
+        assert resp.status_code == expected_exception_code
+
+    @pytest.mark.parametrize(
+        "input_id, expected_exception_code",
+        [
+            (1, 204),  # success
+            (-1, 400),  # invalid id
+            (12345, 404),  # no product found
+            ("abc", 400),  # invalid id
+        ],
+    )
+    def test_delete_product(
+        self, client, auth_tokens, input_id, expected_exception_code
+    ):
+
+        headers = auth_header(auth_tokens, "admin")
+        product = {
+            "description": "Test product",
+            "barcode": "123456789012",
+            "price_per_unit": 9.99,
+            "note": "",
+            "quantity": 10,
+            "position": "A-1-1",
+        }
+
+        client.post(
+            BASE_URL + "/products",
+            json=product,
+            headers=headers,
+        )
+
+        resp = client.delete(
+            BASE_URL + "/products/" + str(input_id),
+            headers=headers,
+        )
+
+        if resp.status_code == 422:
+            resp.status_code = 400
+
+        assert resp.status_code == expected_exception_code
+
+    @pytest.mark.parametrize(
+        "product_id, product_update, expected_exception_code",
+        [
+            (
+                1,  # correct update
+                {
+                    "description": "Test product",
+                    "barcode": "9780201379624",
+                    "price_per_unit": 9.99,
+                    "note": "",
+                    "quantity": 10,
+                    "position": "A-1-1",
+                },
+                201,
+            ),
+            (
+                -1,  # invalid id
+                {
+                    "description": "Test product",
+                    "barcode": "9780201379624",
+                    "price_per_unit": 9.99,
+                    "note": "",
+                    "quantity": 10,
+                    "position": "A-1-1",
+                },
+                400,
+            ),
+            (
+                1,  # invalid barcode
+                {
+                    "description": "Test product",
+                    "barcode": "12345",
+                    "price_per_unit": 9.99,
+                    "note": "",
+                    "quantity": 10,
+                    "position": "A-1-1",
+                },
+                400,
+            ),
+            (
+                1,  # invalid quantity
+                {
+                    "description": "Test product",
+                    "barcode": "9780201379624",
+                    "price_per_unit": 9.99,
+                    "note": "",
+                    "quantity": -3,
+                    "position": "A-1-1",
+                },
+                400,
+            ),
+            (
+                1,  # invalid price
+                {
+                    "description": "Test product",
+                    "barcode": "9780201379624",
+                    "price_per_unit": -9.99,
+                    "note": "",
+                    "quantity": 10,
+                    "position": "A-1-1",
+                },
+                400,
+            ),
+            (
+                12345,  # not found
+                {
+                    "description": "Test product",
+                    "barcode": "9780201379624",
+                    "price_per_unit": 9.99,
+                    "note": "",
+                    "quantity": 10,
+                    "position": "A-1-1",
+                },
+                404,
+            ),
+            (
+                1,  # barcode already in use
+                {
+                    "description": "Test product",
+                    "barcode": "4006381333931",
+                    "price_per_unit": 9.99,
+                    "note": "",
+                    "quantity": 10,
+                    "position": "A-1-1",
+                },
+                409,
+            ),
+            (
+                1,  # position already in use
+                {
+                    "description": "Test product",
+                    "barcode": "9780201379624",
+                    "price_per_unit": 9.99,
+                    "note": "",
+                    "quantity": 10,
+                    "position": "C-1-1",
+                },
+                409,
+            ),
+        ],
+    )
+    def test_edit_product(
+        self, client, auth_tokens, product_id, product_update, expected_exception_code
+    ):
+        headers = auth_header(auth_tokens, "admin")
+        product = {
+            "description": "Test product",
+            "barcode": "123456789012",
+            "price_per_unit": 9.99,
+            "note": "",
+            "quantity": 10,
+            "position": "B-1-1",
+        }
+
+        product2 = {
+            "description": "Test product 2",
+            "barcode": "4006381333931",
+            "price_per_unit": 9.99,
+            "note": "",
+            "quantity": 10,
+            "position": "C-1-1",
+        }
+
+        client.post(  # create product to update
+            BASE_URL + "/products",
+            json=product,
+            headers=headers,
+        )
+
+        client.post(  # create 2nd product to test conflicts
+            BASE_URL + "/products",
+            json=product2,
+            headers=headers,
+        )
+
+        resp = client.put(
+            BASE_URL + "/products/" + str(product_id),
+            json=product_update,
+            headers=headers,
+        )
+
+        if resp.status_code == 422:
+            resp.status_code = 400
+
+        assert resp.status_code == expected_exception_code
